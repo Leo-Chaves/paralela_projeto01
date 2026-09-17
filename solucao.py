a2a37dcd672e5bd2b6e539f3a149643e2a0cbcf3
Leo-Chaves <99739069+Leo-Chaves@users.noreply.github.com>
2026-09-17 17:15:35 +0000

    .

---
+++ solucao.py
@@ -0,0 +1,131 @@
+import threading
+import time
+
+THREAD_BAIXA = "Thread Baixa"
+THREAD_MEDIA = "Thread Media"
+THREAD_ALTA = "Thread Alta"
+
+barramento_ocupado = False
+mutex = threading.Lock()
+not_busy = threading.Condition(mutex)
+
+cpu_ocupada = False
+prioridade_herdada = False
+
+def registrar_log(nome_thread, msg):
+    print(f"[{time.monotonic()}] [{nome_thread}]: {msg}\n")
+
+def exibir_tempo_total(nome_thread, inicio):
+    tempo_gasto = time.monotonic() - inicio
+    print(f"\n>>> [RESULTADO] A {nome_thread} levou {tempo_gasto:.4f} segundos do inicio ao fim. <<<\n")
+
+def adquirir_barramento(nome_thread, alta_prioridade):
+    global barramento_ocupado
+    global prioridade_herdada
+    not_busy.acquire()
+
+    while barramento_ocupado:
+        if(alta_prioridade):
+            registrar_log(nome_thread, "Barramento ocupado! Aplicando HERANCA DE PRIORIDADE na Thread Baixa.")
+            prioridade_herdada = True
+        else:
+            registrar_log(nome_thread, "Barramento ocupado. Thread vai esperar...")
+        not_busy.wait()
+
+    barramento_ocupado = True
+    registrar_log(nome_thread, "Adquiriu o barramento com sucesso.")
+
+    not_busy.release()
+
+
+def liberar_barramento(nome_thread):
+    global barramento_ocupado
+    global prioridade_herdada
+    not_busy.acquire()
+
+    barramento_ocupado = False
+    prioridade_herdada = False
+
+    registrar_log(nome_thread, "Liberou o barramento e devolveu a prioridade.")
+    not_busy.notify()
+
+    not_busy.release()
+
+def thread_baixa():
+    global cpu_ocupada
+    inicio = time.monotonic()
+    registrar_log(THREAD_BAIXA, "Iniciando...")
+    adquirir_barramento(THREAD_BAIXA, False)
+
+    registrar_log(THREAD_BAIXA, "Realizando escrita lenta no barramento (Dividida em etapas)...")
+
+    for _ in range(5):
+        while cpu_ocupada:
+            registrar_log(THREAD_BAIXA, "CPU está ocupada. Aguardando liberação.")
+            time.sleep(0.05)
+        time.sleep(0.1)
+
+    liberar_barramento(THREAD_BAIXA)
+    registrar_log(THREAD_BAIXA, "Finalizada.")
+    exibir_tempo_total(THREAD_BAIXA, inicio)
+
+
+def thread_media():
+    global cpu_ocupada
+    global prioridade_herdada
+
+    time.sleep(0.05)
+    inicio = time.monotonic()
+
+    registrar_log(THREAD_MEDIA, "Iniciando. PREEMPTANDO a Baixa e Monopolizando a CPU...")
+
+    while(prioridade_herdada):
+        registrar_log(THREAD_MEDIA, "BLOQUEADA! Thread Baixa esta com prioridade ALTA.")
+        time.sleep(0.05)
+
+    cpu_ocupada = True
+
+    contador = 0
+    # Mesmo peso de processamento (50 milhões) para a comparação ser justa
+    for i in range(50000000):
+        contador += 1
+
+    cpu_ocupada = False
+
+    registrar_log(THREAD_MEDIA, "Finalizou calculos intensivos. Devolveu a CPU.")
+    exibir_tempo_total(THREAD_MEDIA, inicio)
+
+
+def thread_alta():
+    time.sleep(0.02)
+    inicio = time.monotonic()
+
+    registrar_log(THREAD_ALTA, "Iniciando requisicao critica de tempo real...")
+    adquirir_barramento(THREAD_ALTA, True)
+
+    registrar_log(THREAD_ALTA, "Enviando dados...")
+    time.sleep(0.1)
+    liberar_barramento(THREAD_ALTA)
+
+    registrar_log(THREAD_ALTA, "Finalizada.")
+    exibir_tempo_total(THREAD_ALTA, inicio)
+
+
+def main():
+    print("--- INICIANDO SIMULADOR MARS PATHFINDER (COM SOLUCAO) ---\n")
+
+    tb = threading.Thread(target=thread_baixa)
+    ta = threading.Thread(target=thread_alta)
+    tm = threading.Thread(target=thread_media)
+
+    tb.start()
+    ta.start()
+    tm.start()
+
+    tb.join()
+    ta.join()
+    tm.join()
+
+
+if __name__ == "__main__":
+    main()
